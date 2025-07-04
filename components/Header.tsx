cb60086e614d2f89fda95512ed24af3422004ab3
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-07-05 00:23:12 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -116,7 +116,7 @@ const Header: React.FC = () => {
           <div className="flex items-center space-x-3">
             <AshokaChakraIcon className="h-10 w-10 text-bharat-blue-800 rotate-slow" />
             <h1 className="text-xl md:text-2xl font-extrabold text-bharat-blue-900 tracking-tight">
-              {/*Bharat Mitra */}
+              Bharat Mitra
             </h1>
           </div>
 
