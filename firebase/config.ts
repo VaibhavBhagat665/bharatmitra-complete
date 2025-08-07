14ff39430c2fd5d1ddd510d62eaa9217daf79051
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-08-07 11:32:09 +0000

    Minor key bug

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,17 +3,16 @@ import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } 
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvHJJN7fhDPJJa_UZsBTyX9QYDj4t4vu4",
-  authDomain: "bharat-base.firebaseapp.com",
-  projectId: "bharat-base",
-  storageBucket: "bharat-base.firebasestorage.app",
-  messagingSenderId: "1015918053215",
-  appId: "1:1015918053215:web:6065d65edccaee6c12d8ea",
-  measurementId: "G-4QK76486QW"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
